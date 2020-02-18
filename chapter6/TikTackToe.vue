290e780b207a8b803990bae54702b6a8386c30e6
WooYoo <puba5@naver.com>
2020-02-18 21:03:07 +0900

    틱택토 이벤트 버스를 이용하도록 수정

--- chapter6/TikTackToe.vue
+++ chapter6/TikTackToe.vue
@@ -2,11 +2,12 @@
   <div>
     <table-component :table-data="tableData"/>
     <div v-if="!winner">{{turns}}의 차례입니다</div>
-    <div v-if="winner">{{winner}}님의 승리입니다</div>
+    <div v-if="winner">{{winner}}님이 승리하셨습니다!! 2초 뒤 새 게임이 시작됩니다</div>
   </div>
 </template>
 <script>
 import TableComponent from "./TableComponent";
+import EventBus from "./Eventbus";
 export default {
   components: {
     TableComponent
@@ -15,8 +16,79 @@ export default {
     return {
       tableData: [["", "", ""], ["", "", ""], ["", "", ""]],
       turns: "O",
-      winner: ""
+      winner: "",
+      rowIndex: null,
+      cellIndex: null
     };
+  },
+  methods: {
+    onclickTd(rowIndex, cellIndex) {
+      if (this.tableData[rowIndex][cellIndex] != "") {
+        return;
+      }
+      this.$set(this.tableData[rowIndex], cellIndex, this.turns);
+      var win = false;
+      var all = true;
+      if (
+        this.tableData[0][cellIndex] == this.tableData[1][cellIndex] &&
+        this.tableData[1][cellIndex] == this.tableData[2][cellIndex] &&
+        this.tableData[2][cellIndex] == this.turns
+      ) {
+        win = true;
+      }
+      if (
+        this.tableData[rowIndex][0] == this.tableData[rowIndex][1] &&
+        this.tableData[rowIndex][1] == this.tableData[rowIndex][2] &&
+        this.tableData[rowIndex][2] == this.turns
+      ) {
+        win = true;
+      }
+      if (
+        this.tableData[0][0] == this.tableData[1][1] &&
+        this.tableData[1][1] == this.tableData[2][2] &&
+        this.tableData[0][0] == this.turns
+      ) {
+        win = true;
+      }
+      if (
+        this.tableData[0][2] === this.tableData[1][1] &&
+        this.tableData[1][1] == this.tableData[2][0] &&
+        this.tableData[0][2] == this.turns
+      ) {
+        win = true;
+      }
+      if (win) {
+        this.winner = this.turns;
+        var timeout = setTimeout(() => {
+          this.winner = "";
+          this.turns = "O";
+          this.tableData = [["", "", ""], ["", "", ""], ["", "", ""]];
+        }, 2000);
+      } else {
+        this.tableData.forEach(row => {
+          row.forEach(cell => {
+            if (!cell) {
+              all = false;
+            }
+          });
+        });
+        if (all) {
+          //무승부인 경우
+          this.winner = "";
+          this.turns = "O";
+          this.tableData = [["", "", ""], ["", "", ""], ["", "", ""]];
+          return;
+        }
+      }
+
+      this.turns = this.turns === "O" ? "X" : "O";
+    }
+  },
+  created() {
+    EventBus.$on("clickTd", this.onclickTd);
+  },
+  beforeDestory() {
+    clearTimeout(timeout);
   }
 };
 </script>
---
+++ readme.md
@@ -0,0 +1,9 @@
+# Web games with vue
+
+> 1. 구구단 - chapter1
+> 2. 끝말잇기 - chapter1
+> 3. 숫자야구 - chapter2
+> 4. 반응속도 체크 - chapter3
+> 5. 가위바위보 - chapter4
+> 6. 로또 - chapter5
+> 7. 틱택토 - chapter6
